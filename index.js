bf611fe240dd3b2c6102854f4cfe457df6b93956
Le Van Thao <vanthaodhdt@gmail.com>
2020-07-04 14:55:46 +0700

    fix port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
